85fb814b13a25e1c401c19ab267b257006007091
Hwangzii <nhpphi2002@gmail.com>
2024-03-10 16:27:18 +0700

    hazi

--- loginPage/loginPage/trangchu.xaml.cs
+++ loginPage/loginPage/trangchu.xaml.cs
@@ -11,17 +11,48 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static MaterialDesignThemes.Wpf.Theme;
 
 namespace loginPage
 {
-    /// <summary>
-    /// Interaction logic for trangchu.xaml
-    /// </summary>
+
     public partial class trangchu : Window
     {
+        
+
+        // FIX LAI CODE
+
         public trangchu()
         {
             InitializeComponent();
+            
+        }
+
+       
+
+        private void comboBoxMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Xóa các UserControl hiện tại trong pageContainer
+            pageContainer.Children.Clear();
+
+            // Lấy ComboBoxItem đã chọn
+            ComboBoxItem selectedItem = comboBoxMenu.SelectedItem as ComboBoxItem;
+
+            // Kiểm tra ComboBoxItem đã chọn và hiển thị Page tương ứng
+            if (selectedItem != null)
+            {
+                if (selectedItem.Content.ToString() == "Món nóng")
+                {
+                    // Hiển thị Page1
+                    
+                    page1 page1 = new page1();
+                    pageContainer.Children.Add(page1);
+                }
+                // Xử lý các ComboBoxItem khác ở đây
+            }
         }
     }
+
+
+    
 }
