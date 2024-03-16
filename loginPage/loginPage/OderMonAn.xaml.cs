83f589ef4b4ae98d13e4a2a10faa1302cdcf3494
VXN <zennvader001@gmail.com>
2024-03-16 22:27:01 +0700

    Sua lai cac Button mon an

--- loginPage/loginPage/OderMonAn.xaml.cs
+++ loginPage/loginPage/OderMonAn.xaml.cs
@@ -21,7 +21,7 @@ namespace loginPage
     /// </summary>
     public partial class OderMonAn : Window
     {
-        string connectstring = @"Data Source=HOANGPHI;Initial Catalog=restaurant_DB_vxn;Integrated Security=True";
+        string connectstring = @"Data Source=DESKTOP-BTLUTR6\SQLEXPRESS;Initial Catalog=restaurant_DB;Integrated Security=True;Encrypt=False";
         public OderMonAn()
         {
             InitializeComponent();
@@ -29,7 +29,7 @@ namespace loginPage
 
         private void goimonTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (MonNuongTabItem.IsSelected)
+            if (AllTabItem.IsSelected)
             {
                 int maMonAn = 0;
                 SqlConnection conn = new SqlConnection(connectstring);
@@ -40,7 +40,7 @@ namespace loginPage
                     while (read.Read())
                     {
                         maMonAn = (int)read["MaMA"];
-                        for (int i = 1; i < 35; i++)
+                        for (int i = 1; i <= 35; i++)
                         {
                             Button dynamicBtn = new Button();
                             StackPanel dynamicStp = new StackPanel();
@@ -87,8 +87,74 @@ namespace loginPage
                                 Grid.SetColumn(dynamicBtn, column);
                                 Grid.SetRow(dynamicBtn, row);
 
-                                monNuongGrid.Children.Add(dynamicBtn);
+                                AllGrid.Children.Add(dynamicBtn);
                             }
+                        }
+                    }
+                }
+            }
+
+            else if (MonNuongTabItem.IsSelected)
+            {
+                int maMonAn = 0;
+                SqlConnection conn = new SqlConnection(connectstring);
+                SqlCommand command = new SqlCommand("SELECT * FROM monan_TB", conn);
+                conn.Open();
+                using (SqlDataReader read = command.ExecuteReader())
+                {
+                    while (read.Read())
+                    {
+                        maMonAn = (int)read["MaMA"];
+                        for (int i = 1; i <= 10; i++)
+                        {
+                            Button dynamicBtn = new Button();
+                            StackPanel dynamicStp = new StackPanel();
+                            Border dynamicBorder = new Border();
+                            TextBlock dynamicTxtTenMonAn = new TextBlock();
+                            TextBlock dynamicTxtGiaMonAn = new TextBlock();
+                            Image dynamicImg = new Image();
+                            string foodImageURL = string.Format(@"/Images/diet.png", i);
+                            BitmapImage foodImg = new BitmapImage(new Uri(foodImageURL, UriKind.Relative));
+                            dynamicImg.Source = foodImg;
+                            dynamicImg.Width = 150;
+                            dynamicImg.Height = 150;
+
+                            int row = (i - 1) / 5;
+                            int column = (i - 1) % 5;
+
+                            if (maMonAn == i)
+                            {
+                                dynamicTxtTenMonAn.Text = i.ToString() + ".  " + read["TenMA"].ToString();
+                                dynamicTxtTenMonAn.HorizontalAlignment = HorizontalAlignment.Center;
+                                dynamicTxtTenMonAn.VerticalAlignment = VerticalAlignment.Bottom;
+                                dynamicTxtTenMonAn.Margin = new Thickness(0, 0, 0, 0);
+                                dynamicTxtTenMonAn.FontSize = 20;
+                                dynamicTxtTenMonAn.TextWrapping = TextWrapping.Wrap;
+
+                                dynamicTxtGiaMonAn.Text = read["DonGia"].ToString() + " vnđ";
+                                dynamicTxtGiaMonAn.HorizontalAlignment = HorizontalAlignment.Center;
+                                dynamicTxtGiaMonAn.VerticalAlignment = VerticalAlignment.Bottom;
+                                dynamicTxtGiaMonAn.Margin = new Thickness(0, 0, 0, 0);
+                                dynamicTxtGiaMonAn.FontSize = 16;
+                                dynamicTxtGiaMonAn.Foreground = Brushes.Gray;
+                                dynamicTxtGiaMonAn.TextWrapping = TextWrapping.WrapWithOverflow;
+
+                                dynamicStp.Children.Add(dynamicBorder);
+                                dynamicStp.Children.Add(dynamicImg);
+                                dynamicStp.Children.Add(dynamicTxtTenMonAn);
+                                dynamicStp.Children.Add(dynamicTxtGiaMonAn);
+
+                                dynamicBtn.Background = Brushes.White;
+                                dynamicBtn.Margin = new Thickness(0, 10, 10, 0);
+                                dynamicBtn.Content = dynamicStp;
+
+                                Grid.SetColumn(dynamicBtn, column);
+                                Grid.SetRow(dynamicBtn, row);
+
+                                MonNuongGrid.Children.Add(dynamicBtn);
+                            }
+
+
                         }
                     }
                 }
@@ -105,7 +171,7 @@ namespace loginPage
                     while (read.Read())
                     {
                         maMonAn = (int)read["MaMA"];
-                        for (int i = 11; i <= 35; i++)
+                        for (int i = 11; i <= 20; i++)
                         {
                             Button dynamicBtn = new Button();
                             StackPanel dynamicStp = new StackPanel();
@@ -120,7 +186,7 @@ namespace loginPage
                             dynamicImg.Height = 150;
 
                             int row = (i - 11) / 5;
-                            int column = i % 5;
+                            int column = (i - 11) % 5;
 
                             if (maMonAn == i)
                             {
@@ -151,7 +217,7 @@ namespace loginPage
                                 Grid.SetColumn(dynamicBtn, column);
                                 Grid.SetRow(dynamicBtn, row);
 
-                                monLauGrid.Children.Add(dynamicBtn);
+                                MonLauGrid.Children.Add(dynamicBtn);
                             }
 
 
@@ -160,7 +226,7 @@ namespace loginPage
                 }
             }
 
-            else if (monChinTabItem.IsSelected)
+            else if (MonChinTabItem.IsSelected)
             {
                 int maMonAn = 0;
                 SqlConnection conn = new SqlConnection(connectstring);
@@ -171,7 +237,7 @@ namespace loginPage
                     while (read.Read())
                     {
                         maMonAn = (int)read["MaMA"];
-                        for (int i = 11; i <= 35; i++)
+                        for (int i = 21; i <= 30; i++)
                         {
                             Button dynamicBtn = new Button();
                             StackPanel dynamicStp = new StackPanel();
@@ -185,8 +251,8 @@ namespace loginPage
                             dynamicImg.Width = 150;
                             dynamicImg.Height = 150;
 
-                            int row = (i - 11) / 5;
-                            int column = i % 5;
+                            int row = (i - 21) / 5;
+                            int column = (i - 21) % 5;
 
                             if (maMonAn == i)
                             {
@@ -217,7 +283,7 @@ namespace loginPage
                                 Grid.SetColumn(dynamicBtn, column);
                                 Grid.SetRow(dynamicBtn, row);
 
-                                monChinGrid.Children.Add(dynamicBtn);
+                                MonChinGrid.Children.Add(dynamicBtn);
                             }
 
 
@@ -237,7 +303,7 @@ namespace loginPage
                     while (read.Read())
                     {
                         maMonAn = (int)read["MaMA"];
-                        for (int i = 11; i <= 35; i++)
+                        for (int i = 31; i <= 35; i++)
                         {
                             Button dynamicBtn = new Button();
                             StackPanel dynamicStp = new StackPanel();
@@ -251,8 +317,8 @@ namespace loginPage
                             dynamicImg.Width = 150;
                             dynamicImg.Height = 150;
 
-                            int row = (i - 11) / 5;
-                            int column = i % 5;
+                            int row = (i - 31) / 5;
+                            int column = (i - 31) % 5;
 
                             if (maMonAn == i)
                             {
